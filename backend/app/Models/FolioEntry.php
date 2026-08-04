917370f6a087c521067ffdfc526e5c4aca1427e3
UKEJ <edehkelvinjunior@gmail.com>
2026-08-04 23:50:13 +0100

    refining stayflow/backend/app/models

--- backend/app/Models/FolioEntry.php
+++ backend/app/Models/FolioEntry.php
@@ -1,5 +1,5 @@
 <?php
-
+// Folioentry.php
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
--- backend/app/Models/Guest.php
+++ backend/app/Models/Guest.php
@@ -1,5 +1,5 @@
 <?php
-
+// Guest.php
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
