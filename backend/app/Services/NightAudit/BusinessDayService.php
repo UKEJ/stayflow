463a1fce9bab1fb5690c2525c227f36f6918c275
UKEJ <edehkelvinjunior@gmail.com>
2026-08-21 23:55:34 +0100

    null

--- backend/app/Services/NightAudit/BusinessDayService.php
+++ backend/app/Services/NightAudit/BusinessDayService.php
@@ -8,7 +8,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class BusinessDayService
-{
+{     
     public function current(Business $business): BusinessDay
     {
         $current = BusinessDay::where('business_id', $business->id)
